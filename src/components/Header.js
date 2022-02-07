d7e7b56bd08dc4882696c816fe956ed7d4f428a6
canosaer <67963828+canosaer@users.noreply.github.com>
2022-02-06 22:01:53 -0500

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,14 +16,16 @@ export default function Header() {
     return(
         <section className="header">
             <button className="toggle" onClick={() => setMenuOpen(!menuOpen)}><FontAwesomeIcon className="toggle__bars" icon="bars" /></button>
+            <div className="logo__container">
             <button className="logo" onClick={() => navigate('/')}><img className="logo__image" src="https://www.zmangames.com/static/images/zman_white_logo.8bc6e70a08d3.svg" alt="logo"/></button>
+            </div>
             <button className="cart" onClick={() => navigate('/cart')}><FontAwesomeIcon className="cart__image" icon="shopping-cart" /></button>
 
             <aside className={sidebarStyles}>
                 <ul className="sidebar__list">
                     <li className="sidebar__item"><a className="sidebar__link" href="">Home</a></li>
-                    <li className="sidebar__item"><a className="sidebar__link" href="">Link1</a></li>
-                    <li className="sidebar__item"><a className="sidebar__link" href="">Link2</a></li>
+                    <li className="sidebar__item"><a className="sidebar__link" href="">Shop</a></li>
+                    <li className="sidebar__item"><a className="sidebar__link" href="">Cart</a></li>
                 </ul>
             </aside>
 
