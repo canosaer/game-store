f5a742a341d9d0d6ce09ea7913e39b43086e7875
canosaer <67963828+canosaer@users.noreply.github.com>
2022-03-13 22:44:24 -0400

    1.0

--- src/views/Cart.js
+++ src/views/Cart.js
@@ -20,25 +20,33 @@ export default function Cart() {
     return(
         <div>
             <Header />
-            <h1>Cart</h1>
+            <h2 className="shop__heading">Cart</h2>
+            <div className="shop__gallery-container">
+                <section className="shop__gallery">
+                    {Object.entries(visualCart).map((p, i) => {
+                        const key = `cart-item--${i}`
+                        const product = p[1][0]
+                        return (
+                            <div className="product" key={key}>
+                                <img className="product__image" src={product.image} alt={product.title} />
+                                <div className="product__details">
+                                    <h4 className="product__title">{product.title}</h4>
+                                    <p className="product__desc">Item No. {product.id}</p>
+                                    <strong className="product__price">{product.price}</strong>
+                                    <div className="product__controls">
+                                        <button className="product__remove" onClick={() => removeItem(product)}>-</button>
+                                        <span className="product__num">x{p[1].length}</span>
+                                        <button className="product__add" onClick={() => addItemToCart(product, state, dispatch, uid)}>+</button>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        )
+                    })}
+                </section>
+            </div>
         
-            {Object.entries(visualCart).map((p, i) => {
-                console.log(p[1])
-                const key = `cart-item--${i}`
-                const product = p[1][0]
-                return (
-                    <div className="item" key={key}>
-                        <img src={product.image} alt={product.title} />
-                        <h4>{product.title}</h4>
-                        <p>{product.desc}</p>
-                        <strong>Price: {product.price}</strong>
-                        <span>x {p[1].length}</span>
-                        <button onClick={() => removeItem(product)}>-</button>
-                        <button onClick={() => addItemToCart(product, state, dispatch, uid)}>+</button>
-                    </div>
-                )
-
-           })}
+            
 
         </div>
     )
