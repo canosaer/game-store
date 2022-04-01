8ca0ddd235d7664e770781445bab880c4c7ed73d
canosaer <canosaer@gmail.com>
2022-04-01 17:16:16 -0400

    carousel progress

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,50 +1,57 @@
 import {useEffect, useLayoutEffect, useRef, useState} from 'react';
 import { useWindowSize } from '../hooks/useWindowSize';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { gsap } from "gsap";
 
 export default function Carousel() {
-  const [slideIndex, setSlideIndex] = useState(0);
-  const [isScrolling, setIsScrolling] = useState(true);
-  const [slideBounds, setSlideBounds] = useState();
-  const windowSize = useWindowSize();
-
-  const slidesEl = useRef(null);
-  const slideEls = useRef(null);
-
+  const [slideIndex, setSlideIndex] = useState(0)
+  const [ready, setReady] = useState(true)
+  const [pause, setPause] = useState(false)
+  const [outgoingSlide, setOutgoingSlide] = useState(0)
+  
   const slides = [
     {   imageClass: 'carousel__image carousel__image_jabba',
         textShapeClass: 'carousel__text-shape carousel__text-shape_jabba',
         heading: "Star Wars: Jabba's Palace - A Love Letter Game",
         text: 'A quick card game of rebel bravery and vile deceit.',
         gradientClass: 'carousel__gradient carousel__gradient_jabba',
+        ref: useRef()
     },
     {   imageClass: 'carousel__image carousel__image_citadels',
         textShapeClass: 'carousel__text-shape carousel__text-shape_citadels',
         heading: "Citadels",
         text: 'Build a medieval city fit for a king in this definitive edition of the acclaimed card game.',
         gradientClass: 'carousel__gradient carousel__gradient_citadels',
+        ref: useRef()
     },
     {   imageClass: 'carousel__image carousel__image_cryo',
         textShapeClass: 'carousel__text-shape carousel__text-shape_cryo',
         heading: "Cryo",
         text: "With your colony ship wrecked on a remote frozen planet, it's up to you to scavenge, build, explore, and lead your faction to victory before the sun sets.",
         gradientClass: 'carousel__gradient carousel__gradient_cryo',
+        ref: useRef()
     },
-
-
-
   ]
-        
+
+      
     // Go to slide index.
   const goToSlide = (index) => {
-    setIsScrolling(false);
-    setSlideIndex(index);
+    setReady(false)
+    console.log(outgoingSlide, slideIndex)
+    if((slideIndex === 0 && outgoingSlide === slides.length-1) || slideIndex > outgoingSlide) gsap.from(slides[index].ref.current, {x:'100%'})
+    else if(slideIndex===slides.length-1 && outgoingSlide === 0 || slideIndex < outgoingSlide) gsap.from(slides[index].ref.current, {x:'-100%'})
+    
+    
+    
+
   };
   
   // Go to previous  slide.
   const goToPreviousSlide = (index) => {
+    setOutgoingSlide(slideIndex)
     const prevIndex = index - 1 >= 0 ? index - 1 : slides.length - 1;
-    goToSlide(prevIndex);
+    setSlideIndex(prevIndex)
+    goToSlide(slideIndex);
   };
 
   // Go to next slide.
@@ -53,54 +60,28 @@ export default function Carousel() {
     goToSlide(nextIndex);
   };
 
-  // Update index when the slidesEl is scrolled.
-  const onScroll = () => {
-    if (slidesEl.current && slideBounds && isScrolling) {
-      const newIndex = Math.round(
-        slidesEl.current.scrollLeft / slideBounds.width
-      );
-      setSlideIndex(newIndex);
-    }
-  };
-
   const pauseShow = () => {
 
   }
-
-  // When the index changes, scroll the div to the appropriate index.
-  useEffect(() => {
-    if (slideBounds && slidesEl.current) {
-      slidesEl.current.scrollTo({
-        top: 0,
-        left: slideBounds.width * slideIndex,
-        behavior: 'smooth',
-      });
-      return;
-    }
-  }, [slideBounds, slidesEl, slideIndex]);
-
-  // If window is resized, reset slideBounds.
-  useLayoutEffect(() => {
-    if (windowSize) {
-      setSlideBounds(slideEls.current?.getBoundingClientRect());
-      return;
-    }
-  }, [slideEls, windowSize]);
-
-  // Set slideBounds on mount.
-  useLayoutEffect(() => {
-    setSlideBounds(slideEls.current?.getBoundingClientRect());
-    return;
-  }, []);
 
   return (
     <section className="carousel">
         {slides.map((slide, i) => {
             const key = `slide-${i}`;
-            const displayClass = slideIndex == i ? 'carousel__slide displayed' : 'carousel__slide hidden'
+            let displayClass = ''
+
+            if(slideIndex === i){
+              displayClass = 'carousel__slide displayed bring-to-front'
+            }
+            else if(!ready && i===outgoingSlide){
+              displayClass = 'carousel__slide displayed'
+            }
+            else{
+              displayClass = 'carousel__slide hidden'
+            }       
 
             return (
-                <div className={displayClass} ref={slideEls} key={key}>
+                <div className={displayClass} key={key} ref={slide.ref}>
                     <div className="hero__image-slot">
                         <figure className={slide.imageClass}>
                             <div className={slide.textShapeClass}>
